1347d5d727058b6cd0b95eec6b73fc2da372fcff
mrostamali <maryam.rostamali91@gmail.com>
2021-01-13 10:16:13 -0700

    create first sample console program

---
+++ MyFirstPlan/MyFirstPlan/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstPlan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The first step is buying all the ingredient from the store");
+            Console.WriteLine("The second step is slice the chicken and tomato");
+            Console.WriteLine("The third step is put the chicken in the oven for 30 min in 370 F");
+            Console.WriteLine("The fourth step is Spread the sauce on the bread");
+            Console.WriteLine("The fifth step is put the chicken and tomato on the bread");
+        }
+    }
+}
